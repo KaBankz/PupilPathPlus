9ff10133432c6dd081e3533915996414f75621e3
DeathHackz <xdeathhackzx@gmail.com>
2018-05-23 21:00:12 -0400

    Release v1.2

--- PupilPathPlus.user.js
+++ PupilPathPlus.user.js
@@ -1,8 +1,9 @@
 // ==UserScript==
 // @name         PupilPath Plus
 // @namespace    https://github.com/DeathHackz
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // @icon         http://icons.iconarchive.com/icons/dtafalonso/android-lollipop/512/Calculator-icon.png
-// @version      1.0
+// @version      1.2
 // @description  Appends Total Average to PupilPath
 // @author       DeathHackz
 // @include      https://pupilpath.skedula.com/*
@@ -17,6 +18,7 @@
         if ($('#totalaverage').length === 0) {
             $('.information').prepend("<a id='avgli' href='#'><p style='position: static;float: right;padding-top: 10px;padding-right: 10px;'>Total Average: <img id='avgimg' src='' /><span id='totalaverage'></span></p></a>");
         }
+        
         var links = document.querySelectorAll('tr[style="cursor: pointer"]');
         var One = "";
         var Two = "";
@@ -24,42 +26,42 @@
         var Four = "";
         var num = 0;
         var count = 0;
+        
         for (var i = 0; i < links.length; i++) {
             var str = links[i].innerText;
             One = str.split('MP1:  ')[1];
             One = parseFloat(One);
             One = One || 0;
-            if (One === 0) {} else {
+            if (One == 0) {} else {
                 num += parseFloat(One);
                 count++;
             }
             Two = str.split('MP2:  ')[1];
             Two = parseFloat(Two);
             Two = Two || 0;
-            if (Two === 0) {} else {
+            if (Two == 0) {} else {
                 num += parseFloat(Two);
                 count++;
             }
             Three = str.split('MP3:  ')[1];
             Three = parseFloat(Three);
             Three = Three || 0;
-            if (Three === 0) {} else {
+            if (Three == 0) {} else {
                 num += parseFloat(Three);
                 count++;
             }
             Four = str.split('MP4:  ')[1];
             Four = parseFloat(Four);
             Four = Four || 0;
-            if (Four === 0) {} else {
+            if (Four == 0) {} else {
                 num += parseFloat(Four);
                 count++;
             }
         }
+        
         var avg = num / count;
         var finalavg = Math.ceil(avg * 100) / 100;
         var avgimg;
-
-
         var avgfix = Number(finalavg);
 
         if (avgfix >= 90) {
@@ -82,6 +84,7 @@
             $('#totalaverage').text(" " + finalavg);
             $('#avgimg').attr("src", avgimg);
         }
+
     }
 
 })();
